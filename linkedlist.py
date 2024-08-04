414bdf55ca79fc58f6c4b57973b803321bdf20f0
suvarna1228 <suvu1228@gmail.com>
2024-08-04 16:06:01 +0530

    lerned deletion from linked list at end

--- linkedlist.py
+++ linkedlist.py
@@ -67,15 +67,21 @@ class linkedlist:
             print("ll is empty so we cant delete nodes!")
         else:
             self.head = self.head.ref 
+    def delete_end(self):
+        if self.head is None:
+            print("ll is empty so we cant delete nodes!")
+        elif self.head.ref is None:
+            self.head= None 
+        else:
+            n = self.head
+            while n.ref.ref is not None:
+                n = n.ref
+            n.ref= None
 
 ll1=linkedlist()
 ll1.add_begin(10)
 ll1.add_begin(20)
 ll1.add_end(100)
 ll1.add_end(500)
-ll1.insert_empty(10)
-ll1.add_begin(30)
-ll1.add_after(200,100)
-ll1.add_before(20,10)
-ll1.delete_begin()
+ll1.delete_end()
 ll1.print_ll()
