55b137d78d83b09d406e24e3c12117e1ed03d99d
suvarna1228 <suvu1228@gmail.com>
2024-08-08 14:07:13 +0530

    Learned Isertion from begin and end In Doubly Linkedlist

--- doubly_linkedlist.py
+++ doubly_linkedlist.py
@@ -2,7 +2,7 @@ class Node:
     def __init__(self,data):
         self.data = data
         self.nref = None
-        self.nref = None
+        self.pref = None
 
 class doublyLL:
     def __init__(self):
@@ -10,24 +10,24 @@ class doublyLL:
 
     def print_ll(self) :
         if self.head is None:
-            print("the linked list is empty!")
+            print(" linked list is empty!")
         else:
-           n=self.head
+           n = self.head
            while n is not None:
-               print(n.data)
-               n=n.nref
+               print(n.data, "-->",end="")
+               n = n.nref
 
     def print_ll_reverse(self) :
+        print()
         if self.head is None:
-            print("the linked list is empty!")
+            print("linked list is empty!")
         else:
-           n=self.head
+           n = self.head
            while n.nref is not None:
-               print(n.data)
-               n=n.nref
-           while n.pref is not None:
-               print(n.data)
-               n=n.pref
+               n = n.nref
+           while n is not None:
+               print(n.data, "-->",end=" ")
+               n = n.pref
     def insert_empty(self,data):
         if self.head is None:
             new_node = Node(data)
@@ -35,6 +35,26 @@ class doublyLL:
         else:
             print("linked list is not empty!")
 
+    def add_begin(self,data):
+        new_node=Node(data)
+        if self.head is None:
+           self.head=new_node
+        else:
+            new_node.nref= self.head
+            self.head.pref = new_node
+            self.head = new_node
+    def add_end(self,data):
+        new_node=Node(data)
+        if self.head is None:
+           self.head=new_node
+        else:
+            n = self.head
+            while n.nref is not None:
+               n= n.nref
+            n.nref = new_node
+            new_node.pref = n
 dl1 = doublyLL()
 dl1.insert_empty(10)
+dl1.insert_empty(20)
+dl1.print_ll_reverse()
 dl1.print_ll()
