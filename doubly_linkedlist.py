71eff043e8bf618094a4853fa5caba7b8049f012
suvarna1228 <suvu1228@gmail.com>
2024-08-10 11:28:46 +0530

    Learned insertion operation after and before a  node in Doubly Linkedlist

--- doubly_linkedlist.py
+++ doubly_linkedlist.py
@@ -53,8 +53,51 @@ class doublyLL:
                n= n.nref
             n.nref = new_node
             new_node.pref = n
+    def add_end(self,data,x):
+        if self.head is None:
+            print("linked list is empty!")
+        else:
+           n = self.head
+           while n is not None: 
+               if x==n.data:
+                   break
+           if n is None:
+               print("Given node is not present in dll")
+           else:
+             new_node=Node(data)
+             new_node.nref = n.nref
+             new_node.pref = n
+             if n.nref is not None:
+                 n.nref.pref=new_node
+             n.nref=new_node
+    def add_before(self,data,x):
+        if self.head is None:
+            print("linked list is empty!")
+        else:
+           n = self.head
+           while n is not None: 
+               if x==n.data:
+                   break
+           if n is None:
+               print("Given node is not present in dll")
+           else:
+            new_node = Node(data)
+            new_node.pref = n
+            new_node.nref = n.nref
+            if n.pref is not None:
+                n.pref.nref = new_node
+            else:
+                self.head = new_node
+                n.pref = new_node
+                                              
+
+
+
+
+
+
 dl1 = doublyLL()
-dl1.insert_empty(10)
-dl1.insert_empty(20)
+dl1.add_begin(4)
+dl1.add_before(10,4)
 dl1.print_ll_reverse()
 dl1.print_ll()
